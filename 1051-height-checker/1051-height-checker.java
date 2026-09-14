88935d08f31ce4cabbdee84f05a50d9ef4146907
keshavsharma9259-gif <keshavsharma9259@gmail.com>
2026-09-14 19:35:57 +0530

    Time: 4 ms (77.85%), Space: 43.2 MB (84.94%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int heightChecker(int[] heights) {
+
+        int[] copy = heights.clone();
+
+        Arrays.sort(copy);
+
+        int count = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != copy[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
